f26848392e65bae91908f6d07163bd1d1ae1832f
Shii_039 <enshikuku39@gmail.com>
2022-08-16 13:39:03 +0300

    added validations: password mismatch

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+const users = [
+    { 
+        name: 'Emma', 
+        email: 'en@shi.com', 
+        password: '1234567890' 
+    }, 
+    { 
+        name: 'Kenya', 
+        email: 'ke@gmail.com', 
+        password: '987654321' 
+    },
+    { 
+        name: 'tree', 
+        email: 're@shi.com', 
+        password: '6789054321' 
+    }
+]
 // Landing page
 app.get('/', (req, res) => {
     res.render('index')
@@ -14,7 +32,34 @@ app.get('/login', (req, res) => {
 })
 // Display Signup Page
 app.get('/signup', (req, res) => {
-    res.render('signup')
+    const user = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+    res.render('signup', {error:false, user: user})
+})
+// process signup form 
+app.post('/signup', (req, res) => {
+    const user = {
+        name: req.body.fullname,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+    }
+    if (user.password === user.confirmPassword) {
+        
+    } else {
+        
+        let message = 'Passwords dont match!'
+        res.render('signup', {error:true, message: message, user: user})
+
+    }
+    
+
+    console.log(user)
+    res.send('User account succesfully created')
 })
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
