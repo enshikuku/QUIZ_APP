cdde6fbe7fe2eadcbec2c9ba8fd1a7bd8dff0a7a
Shii_039 <enshikuku39@gmail.com>
2022-08-16 12:43:03 +0300

    created routes: root, login and signup

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ import express from 'express'
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 // Landing page
 app.get('/', (req, res) => {
     res.render('index')
 })
-
+// Display Login Page
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+// Display Signup Page
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`app is running on PORT ${PORT}`)
